df17b773b8fd659d22ead3a9966946067b240fdd
Evan Gunawan <evangunawan@live.com>
2018-07-11 21:28:34 +0700

    Added GhostPatrol Movement
    Sementara ghost random directionnya, sampai ada walls, nanti bisa di atur lagi buat patrolnya.

--- src/pacman_2/Main.java
+++ src/pacman_2/Main.java
@@ -17,227 +17,248 @@ import javax.imageio.ImageIO;
  */
 public class Main extends JFrame implements KeyListener {
 
-    private JPanel pnlMap;
+	private JPanel pnlMap;
 
-    private JPanel pnlMenu;
-    private JLabel lblMenu;
+	private JPanel pnlMenu;
+	private JLabel lblMenu;
 
-    private JLabel lblLife;
-    private JLabel lblTitleScore;
-    private JLabel lblScore;
-    private JLabel lblAlias;
+	private JLabel lblLife;
+	private JLabel lblTitleScore;
+	private JLabel lblScore;
+	private JLabel lblAlias;
 
-//    private JLabel lblPacmanIcon;
-    private JLabel lblBlinkyIcon;
-    private JLabel lblPinkyIcon;
-    private JLabel lblInkyIcon;
-    private JLabel lblClydeIcon;
+	// private JLabel lblPacmanIcon;
 
-    private Player lblPacmanIcon;
+	private GhostHoming lblPinkyIcon;
+	private GhostPatrol lblBlinkyIcon;
+	private GhostMovementHandler blinkyMH;
+	private GhostPatrol lblInkyIcon;
+	private GhostMovementHandler inkyMH;
+	private GhostPatrol lblClydeIcon;
+	private GhostMovementHandler clydeMH;
 
-    public Main() {
-        initComponents();
-    }
+	private Player lblPacmanIcon;
 
-    private Image resizeImage(String url, int width, int height) {
-        Image dimg = null;
-        try {
-            BufferedImage img = ImageIO.read(new File(url));
-            dimg = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-        } catch (IOException ex) {
-            ex.printStackTrace(System.err);
-        }
-        return dimg;
-    }
+	public Main() {
+		initComponents();
+	}
 
-    public void initComponents() {
-        setTitle("Pac-Man");
-        setResizable(false);
-        setUndecorated(true);
-        setSize(448,576);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        getContentPane().setBackground(Color.black);
-        try {
-            this.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("D:\\Latihan\\SP 1\\PBO\\PacMan_2\\Pac-man - Copy.png")))));
-        } catch (IOException ex) {
-            ex.printStackTrace(System.err);
-        }
-        getContentPane().setLayout(null);
+	private Image resizeImage(String url, int width, int height) {
+		Image dimg = null;
+		try {
+			BufferedImage img = ImageIO.read(new File(url));
+			dimg = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+		} catch (IOException ex) {
+			ex.printStackTrace(System.err);
+		}
+		return dimg;
+	}
 
-//        pnlMenu = new JPanel();
-//        pnlMenu.setBounds(0, 0, 464, 615);
-//        pnlMenu.setBackground(Color.black);
-//        pnlMenu.setLayout(null);
-//        pnlMenu.addKeyListener(this);
-//        pnlMenu.setFocusable(true);
-//        add(pnlMenu);
-//
-//        lblMenu = new JLabel("Press any key to start");
-//        lblMenu.setForeground(Color.white);
-//        lblMenu.setFont(new Font("Emulogic", Font.PLAIN, 18));
-//        lblMenu.setBounds(30, 10, 400, 615);
-//        lblMenu.setVisible(true);
-//        pnlMenu.add(lblMenu);
+	public void initComponents() {
+		setTitle("Pac-Man");
+		setResizable(false);
+		setUndecorated(true);
+		setSize(448, 576);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setBackground(Color.black);
+		try {
+			this.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("Pac-man - Copy.png")))));
+		} catch (IOException ex) {
+			ex.printStackTrace(System.err);
+		}
+		getContentPane().setLayout(null);
 
-        pnlMap = new JPanel();
-        pnlMap.setOpaque(false);
-        pnlMap.setLayout(null);
-        pnlMap.setBounds(0, 0, 464, 615);
-        pnlMap.setFocusable(true);
-        pnlMap.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                int x = lblPacmanIcon.getX();
-                int y = lblPacmanIcon.getY();
+		// pnlMenu = new JPanel();
+		// pnlMenu.setBounds(0, 0, 464, 615);
+		// pnlMenu.setBackground(Color.black);
+		// pnlMenu.setLayout(null);
+		// pnlMenu.addKeyListener(this);
+		// pnlMenu.setFocusable(true);
+		// add(pnlMenu);
+		//
+		// lblMenu = new JLabel("Press any key to start");
+		// lblMenu.setForeground(Color.white);
+		// lblMenu.setFont(new Font("Emulogic", Font.PLAIN, 18));
+		// lblMenu.setBounds(30, 10, 400, 615);
+		// lblMenu.setVisible(true);
+		// pnlMenu.add(lblMenu);
 
-                switch (e.getKeyCode()) {
-                    case KeyEvent.VK_UP: {
-                        y -= 1;
-                        if(y > 55){
-                            lblPacmanIcon.arah = 0;
-                        } else {
-                            lblPacmanIcon.arah = lblPacmanIcon.prev_arah;
-                        }
-                        repaint();
-                        break;
-                    }
-                    case KeyEvent.VK_DOWN: {
-                        y += 1;
-                        if(y < 505){
-                             lblPacmanIcon.arah = 1;
-                        } else {
-                            lblPacmanIcon.arah = lblPacmanIcon.prev_arah;
-                        }
-                        
-                        repaint();
-                        break;
-                    }
-                    case KeyEvent.VK_LEFT: {
-                        x -= 1;
-                        if(x > 11){
-                            lblPacmanIcon.arah = 2;
-                        } else {
-                            lblPacmanIcon.arah = lblPacmanIcon.prev_arah;
-                        }
-                        repaint();
-                        break;
-                    }
-                    case KeyEvent.VK_RIGHT: {
-                        x += 1;
-                        if(x > 412){
-                            lblPacmanIcon.arah = 3;
-                        } else {
-                            lblPacmanIcon.arah = lblPacmanIcon.prev_arah;
-                        }
-                        repaint();
-                        break;
-                    }
-                    default:
-                        break;
-                }
-                lblPacmanIcon.setLocation(x, y);
-            }
-        });
-        add(pnlMap);
+		pnlMap = new JPanel();
+		pnlMap.setOpaque(false);
+		pnlMap.setLayout(null);
+		pnlMap.setBounds(0, 0, 464, 615);
+		pnlMap.setFocusable(true);
+		pnlMap.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				int x = lblPacmanIcon.getX();
+				int y = lblPacmanIcon.getY();
 
-        lblLife = new JLabel("Life : ");
-        lblLife.setForeground(Color.white);
-        lblLife.setFont(new Font("Emulogic", Font.PLAIN, 12));
-        lblLife.setBounds(13, 534, 90, 50);
-        pnlMap.add(lblLife);
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_UP: {
+					y -= 1;
+					if (y > 55) {
+						lblPacmanIcon.arah = 0;
+					} else {
+						lblPacmanIcon.arah = lblPacmanIcon.prev_arah;
+					}
+					repaint();
+					break;
+				}
+				case KeyEvent.VK_DOWN: {
+					y += 1;
+					if (y < 505) {
+						lblPacmanIcon.arah = 1;
+					} else {
+						lblPacmanIcon.arah = lblPacmanIcon.prev_arah;
+					}
 
-        lblTitleScore = new JLabel("Score");
-        lblTitleScore.setForeground(Color.white);
-        lblTitleScore.setFont(new Font("Emulogic", Font.PLAIN, 18));
-        lblTitleScore.setBounds(15, 7, 120, 35);
-        pnlMap.add(lblTitleScore);
+					repaint();
+					break;
+				}
+				case KeyEvent.VK_LEFT: {
+					x -= 1;
+					if (x > 11) {
+						lblPacmanIcon.arah = 2;
+					} else {
+						lblPacmanIcon.arah = lblPacmanIcon.prev_arah;
+					}
+					repaint();
+					break;
+				}
+				case KeyEvent.VK_RIGHT: {
+					x += 1;
+					if (x > 412) {
+						lblPacmanIcon.arah = 3;
+					} else {
+						lblPacmanIcon.arah = lblPacmanIcon.prev_arah;
+					}
+					repaint();
+					break;
+				}
+				default:
+					break;
+				}
+				lblPacmanIcon.setLocation(x, y);
+			}
+		});
+		add(pnlMap);
 
-        lblScore = new JLabel("123");
-        lblScore.setForeground(Color.white);
-        lblScore.setFont(new Font("Emulogic", Font.PLAIN, 18));
-        lblScore.setBounds(195, 7, 140, 35);
-        pnlMap.add(lblScore);
+		lblLife = new JLabel("Life : ");
+		lblLife.setForeground(Color.white);
+		lblLife.setFont(new Font("Emulogic", Font.PLAIN, 12));
+		lblLife.setBounds(13, 534, 90, 50);
+		pnlMap.add(lblLife);
 
-        lblAlias = new JLabel("Pac");
-        lblAlias.setForeground(Color.white);
-        lblAlias.setFont(new Font("Emulogic", Font.PLAIN, 18));
-        lblAlias.setBounds(358, 7, 120, 35);
-        pnlMap.add(lblAlias);
+		lblTitleScore = new JLabel("Score");
+		lblTitleScore.setForeground(Color.white);
+		lblTitleScore.setFont(new Font("Emulogic", Font.PLAIN, 18));
+		lblTitleScore.setBounds(15, 7, 120, 35);
+		pnlMap.add(lblTitleScore);
 
-        lblPacmanIcon = new Player();
-        lblPacmanIcon.setIcon(new ImageIcon(resizeImage("D:\\Latihan\\SP 1\\PBO\\PacMan_2\\pac_man_chara.png", 25, 25)));
-        lblPacmanIcon.setBounds(11, 55, 25, 30);
-        pnlMap.add(lblPacmanIcon);
+		lblScore = new JLabel("123");
+		lblScore.setForeground(Color.white);
+		lblScore.setFont(new Font("Emulogic", Font.PLAIN, 18));
+		lblScore.setBounds(195, 7, 140, 35);
+		pnlMap.add(lblScore);
 
-        lblBlinkyIcon = new JLabel();
-        lblBlinkyIcon.setIcon(new ImageIcon(resizeImage("D:\\Latihan\\SP 1\\PBO\\PacMan_2\\blinky.png", 25, 25)));
-        lblBlinkyIcon.setBounds(178, 275, 25, 30);
-        pnlMap.add(lblBlinkyIcon);
+		lblAlias = new JLabel("Pac");
+		lblAlias.setForeground(Color.white);
+		lblAlias.setFont(new Font("Emulogic", Font.PLAIN, 18));
+		lblAlias.setBounds(358, 7, 120, 35);
+		pnlMap.add(lblAlias);
 
-        lblPinkyIcon = new JLabel();
-        lblPinkyIcon.setIcon(new ImageIcon(resizeImage("D:\\Latihan\\SP 1\\PBO\\PacMan_2\\pinky.png", 25, 25)));
-        lblPinkyIcon.setBounds(202, 255, 25, 30);
-        pnlMap.add(lblPinkyIcon);
+		lblPacmanIcon = new Player();
+		lblPacmanIcon.setIcon(new ImageIcon(resizeImage("pac_man_chara.png", 25, 25)));
+		lblPacmanIcon.setBounds(11, 55, 25, 30);
+		pnlMap.add(lblPacmanIcon);
 
-        lblInkyIcon = new JLabel();
-        lblInkyIcon.setIcon(new ImageIcon(resizeImage("D:\\Latihan\\SP 1\\PBO\\PacMan_2\\inky.png", 25, 25)));
-        lblInkyIcon.setBounds(228, 275, 25, 30);
-        pnlMap.add(lblInkyIcon);
+		// Pinky = GhostHoming
+		lblPinkyIcon = new GhostHoming();
+		lblPinkyIcon.setIcon(new ImageIcon(resizeImage("pinky.png", 25, 25)));
+		lblPinkyIcon.setBounds(202, 255, 25, 30);
+		pnlMap.add(lblPinkyIcon);
 
-        lblClydeIcon = new JLabel();
-        lblClydeIcon.setIcon(new ImageIcon(resizeImage("D:\\Latihan\\SP 1\\PBO\\PacMan_2\\clyde.png", 25, 25)));
-        lblClydeIcon.setBounds(247, 255, 25, 30);
-        pnlMap.add(lblClydeIcon);
+		lblBlinkyIcon = new GhostPatrol();
+		blinkyMH = new GhostMovementHandler(lblBlinkyIcon);
+		lblBlinkyIcon.setIcon(new ImageIcon(resizeImage("blinky.png", 25, 25)));
+		lblBlinkyIcon.setBounds(178, 275, 25, 30);
+		pnlMap.add(lblBlinkyIcon);
 
-        Thread pacmanThread = new Thread(lblPacmanIcon);
-        pacmanThread.start();
+		lblInkyIcon = new GhostPatrol();
+		inkyMH = new GhostMovementHandler(lblInkyIcon);
+		lblInkyIcon.setIcon(new ImageIcon(resizeImage("inky.png", 25, 25)));
+		lblInkyIcon.setBounds(228, 275, 25, 30);
+		pnlMap.add(lblInkyIcon);
 
-//        System.out.println(lblPacmanIcon.getX());
-    }
+		lblClydeIcon = new GhostPatrol();
+		clydeMH = new GhostMovementHandler(lblClydeIcon);
+		lblClydeIcon.setIcon(new ImageIcon(resizeImage("clyde.png", 25, 25)));
+		lblClydeIcon.setBounds(247, 255, 25, 30);
+		pnlMap.add(lblClydeIcon);
 
-    public static void main(String[] args) {
-        new Main().setVisible(true);
-    }
+		Thread pacmanThread = new Thread(lblPacmanIcon);
+		pacmanThread.start();
+		
+		Thread blinkytHandler = new Thread(blinkyMH);
+		blinkytHandler.start();
+		
+		Thread inkyHandler = new Thread(inkyMH);
+		inkyHandler.start();
+		
+		Thread clydeHandler = new Thread(clydeMH);
+		clydeHandler.start();
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+		// System.out.println(lblPacmanIcon.getX());
+	}
 
-    @Override
-    public void keyReleased(KeyEvent e) {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	public static void main(String[] args) {
+		new Main().setVisible(true);
+	}
 
-    @Override
-    public void keyPressed(KeyEvent e) {
-        pnlMenu.hide();
-    }
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// throw new UnsupportedOperationException("Not supported yet."); //To change
+		// body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// throw new UnsupportedOperationException("Not supported yet."); //To change
+		// body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		pnlMenu.hide();
+	}
 }
 
-// ============================================= DOODLES ============================================================
-//    private Image resizeImage(String url){
-//        Image dimg = null;
-//        try{
-//            BufferedImage img = ImageIO.read(new File(url));
-//            dimg = img.getScaledInstance(426,547,Image.SCALE_SMOOTH);//426,547
-//        } catch (IOException ex){
-//            ex.printStackTrace(System.err);
-//        }
-//        return dimg;
-//    }
-//        
-//        pnlMap = new JPanel();
-//        pnlMap.setSize(448,576);
-//        pnlMap.setLocation(0,0);
-//        pnlMap.setBackground(Color.black);
-//        add(pnlMap,BorderLayout.NORTH);
-//        lblMap = new JLabel("Name : ");
-//        pnlMap.add(lblMap);
-//        txtText1 = new JTextField(10);
-//        pnlPanel1.add(txtText1);
-//        lblMapIcon = new JLabel();
-//        lblMapIcon.setIcon(new ImageIcon(resizeImage("D:\\Latihan\\SP 1\\PBO\\PacMan_2\\Pac-man - Copy.png")));
-//        lblMapIcon.setLocation(0,0);
-//        pnlMap.add(lblMapIcon);
+// ============================================= DOODLES
+// ============================================================
+// private Image resizeImage(String url){
+// Image dimg = null;
+// try{
+// BufferedImage img = ImageIO.read(new File(url));
+// dimg = img.getScaledInstance(426,547,Image.SCALE_SMOOTH);//426,547
+// } catch (IOException ex){
+// ex.printStackTrace(System.err);
+// }
+// return dimg;
+// }
+//
+// pnlMap = new JPanel();
+// pnlMap.setSize(448,576);
+// pnlMap.setLocation(0,0);
+// pnlMap.setBackground(Color.black);
+// add(pnlMap,BorderLayout.NORTH);
+// lblMap = new JLabel("Name : ");
+// pnlMap.add(lblMap);
+// txtText1 = new JTextField(10);
+// pnlPanel1.add(txtText1);
+// lblMapIcon = new JLabel();
+// lblMapIcon.setIcon(new ImageIcon(resizeImage("D:\\Latihan\\SP
+// 1\\PBO\\PacMan_2\\Pac-man - Copy.png")));
+// lblMapIcon.setLocation(0,0);
+// pnlMap.add(lblMapIcon);
