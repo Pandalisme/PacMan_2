98d9ca6013d7d5ae3c8d3e331b2baa81c8348720
Pandalisme <agapearimatea99@gmail.com>
2018-07-12 22:37:43 +0700

    a

--- src/pacman_2/GhostPatrol.java
+++ src/pacman_2/GhostPatrol.java
@@ -7,60 +7,54 @@ import java.util.Random;
  * @author Agape Arimatea
  */
 public class GhostPatrol extends Ghost implements Runnable {
-	
-	private int direction=0;
 
-	public GhostPatrol() {
+    private int direction = 0;
 
-		
-	}
-	
-	public int getDirection() {
-		return direction;
-	}
-	
-	public void setDirection(int direction) {
-		this.direction = direction;
-	}
+    public GhostPatrol() {
 
+    }
 
+    public int getDirection() {
+        return direction;
+    }
 
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
 
-	@Override
-	public void run() {
-		int posX = this.getX();
-		int posY = this.getY();
-		
-		while(true) {			
-			switch(direction) {
-			case MOVE_UP:
-				posY--;
-				break;
-			case MOVE_RIGHT:
-				posX++;
-				break;
-			case MOVE_DOWN:
-				posY++;
-				break;
-			case MOVE_LEFT:
-				posX--;
-				break;
-			
-			}
-			
-			this.setLocation(posX, posY);
-			repaint();
-			
-			try {
+    @Override
+    public void run() {
+        int posX = this.getX();
+        int posY = this.getY();
+
+        while (true) {
+            switch (direction) {
+                case MOVE_UP:
+                    posY--;
+                    break;
+                case MOVE_RIGHT:
+                    posX++;
+                    break;
+                case MOVE_DOWN:
+                    posY++;
+                    break;
+                case MOVE_LEFT:
+                    posX--;
+                    break;
+
+            }
+
+            this.setLocation(posX, posY);
+            repaint();
+
+            try {
                 Thread.sleep(15);
             } catch (InterruptedException ex) {
                 System.out.println(System.err);
             }
-			
-			
-			
-		}
-		
-	}
-	
+
+        }
+
+    }
+
 }
