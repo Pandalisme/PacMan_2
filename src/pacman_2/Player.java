db6ee4112d7fcee9f377857aa854a32d0cfbfdbb
Pandalisme <agapearimatea99@gmail.com>
2018-07-16 22:05:03 +0700

    Collide With Ghost - Pacman

--- src/pacman_2/Player.java
+++ src/pacman_2/Player.java
@@ -4,19 +4,26 @@ import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.geom.Area;
 import javax.swing.JLabel;
 import java.io.*;
 import javax.imageio.ImageIO;
+import static pacman_2.Main.lblPacmanIcon;
+import static pacman_2.Main.listGhost;
+import static pacman_2.Main.lblLifePoint;
 
 /**
  *
  * @author Agape Arimatea
  */
-public class Player extends Character implements Runnable{
+public class Player extends Character implements Runnable {
 
     private int points;
-    private int life;
+    private int life = 3;
+    private int prev_life = life;
     private boolean powerUp;
+    private int cooldownTime = 2000;
+    private boolean cooldown = false;
     int arah = 3;
     int prev_arah;
 
@@ -26,14 +33,6 @@ public class Player extends Character implements Runnable{
     }
 
     //Action-Methods
-    public void eat(Character target) {
-        if (target instanceof GhostHoming || target instanceof GhostPatrol) {
-            setLife(getLife() - 1);
-            isDead();
-        } else {
-            hitungPoint(target.name);
-        }
-    }
 
     public void hitungPoint(String target) {
         if (target == "Coin") {
@@ -80,6 +79,13 @@ public class Player extends Character implements Runnable{
         this.powerUp = isPowerUp;
     }
 
+    public boolean intersectsGhost(int i) {
+        Area areaA = new Area(lblPacmanIcon.getBounds());
+        Area areaB = new Area(listGhost.get(i).getBounds());
+
+        return areaA.intersects(areaB.getBounds2D());
+    }
+
     @Override
     public void run() {
         int x = this.getX();
@@ -87,26 +93,53 @@ public class Player extends Character implements Runnable{
         this.setVisible(true);
 
         for (int i = 0;; i++) {
+            if (cooldownTime == i) {
+                cooldown = false;
+                System.out.println("cooldown status : " + cooldown);
+            }
+            
             if (arah == 0) {
                 y -= 1;
             } else if (arah == 1) {
                 y += 1;
-            } else if (arah == 2){
+            } else if (arah == 2) {
                 x -= 1;
             } else {
                 x += 1;
             }
-            
-            if(x < 11){
+
+            if (x < 11) {
                 x++;
-            } else if(x > 412){
+            } else if (x > 412) {
                 x--;
-            } else if(y < 55){
+            } else if (y < 55) {
                 y++;
-            } else if(y > 505){
+            } else if (y > 505) {
                 y--;
             }
-            
+
+            if (intersectsGhost(0) && !cooldown) {
+                cooldown = true;
+                System.out.println("Collide with Blinky! : " + i);
+                setLife(getLife() - 1);
+                cooldownTime = i + 400;
+                System.out.println("Life : " + getLife());
+            } else if (intersectsGhost(1) && !cooldown) {
+                cooldown = true;
+                System.out.println("Collide with Inky!   : " + i);
+                setLife(getLife() - 1);
+                System.out.println("Life : " + getLife());
+                cooldownTime = i + 400;
+            } else if (intersectsGhost(2) && !cooldown) {
+                cooldown = true;
+                System.out.println("Collide with Clyde!  : " + i);
+                setLife(getLife() - 1);
+                System.out.println("Life : " + getLife());
+                cooldownTime = i + 400;
+            }
+
+            lblLifePoint.setText(String.valueOf(getLife()));
+
             this.setLocation(x, y);
             repaint();
 
